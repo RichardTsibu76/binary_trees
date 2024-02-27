c371d03cf4420878ea5d961c39472050509cade6
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-02-27 21:05:47 +0100

    This is header files and the prototypes of my various funtion

--- binary_trees.h
+++ binary_trees.h
@@ -92,4 +92,4 @@ int btia_helper(const binary_tree_t *tree, int low, int hi);
 int btih_helper(const binary_tree_t *tree);
 void sata_helper(avl_t **root, int *array, size_t lo, size_t hi);
 
-#endif
+#endif /* _BINARY_TREES_H_ */
