2eb82a5006497b907303521bd9ea5d62ead3430e
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-02-27 22:39:42 +0100

    task zero

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,22 +9,22 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newNode;
+binary_tree_t *newNode;
 
-    /*Dynamically allocating space fot the created node*/
-    newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+/*Dynamically allocating space fot the created node*/
+newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-    if (newNode == NULL)
-    {
-        return (NULL);
-    }
+if (newNode == NULL)
+{
+return (NULL);
+}
 
-    /* assign values to the properties of the node */
-    newNode->parent = parent;
-    newNode->n = value;
-    newNode->left = NULL;
-    newNode->right = NULL;
+/* assign values to the properties of the node */
+newNode->parent = parent;
+newNode->n = value;
+newNode->left = NULL;
+newNode->right = NULL;
 
-    /* return a pointer to the node */
-    return (newNode);
+/* return a pointer to the node */
+return (newNode);
 }
