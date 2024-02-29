31ee6af67fcd8593ae62b0bdd5bc8f5a9692a1cd
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-02-29 10:26:22 +0100

    task updated Hot fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -28,17 +28,17 @@ return ((height_l > height_r) ? height_l : height_r);
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+if (!tree)
+return (0);
 
-    if (binary_height(tree->left) != binary_height(tree->right))
-        return (0);
+if (binary_height(tree->left) != binary_height(tree->right))
+return (0);
 
-    if (!tree->left && !tree->right)
-        return (1);
-    if (tree->left && tree->right)
-        return (binary_tree_is_perfect(tree->left) &&
-                binary_tree_is_perfect(tree->right));
+if (!tree->left && !tree->right)
+return (1);
+if (tree->left && tree->right)
+return (binary_tree_is_perfect(tree->left) &&
+binary_tree_is_perfect(tree->right));
 
-    return (0);
+return (0);
 }
